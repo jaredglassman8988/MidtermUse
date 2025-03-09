eac35b6227812300a14ad2bd857ee37b49b53e63
jaredglassman8988 <jgg8988@nyu.edu>
2025-03-09 15:06:01 +0000

    new file:   IMAGE.jpg new file:   Life Expectancy Data.csv new file:   correlation.jpg new file:   ds.webp new file:   globe.jpg new file:   hi.png new file:   politics.jpg modified:   requirements.txt modified:   streamlit_app.py new file:   wonka.png

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,96 @@
 import streamlit as st
+import seaborn as sns
 import pandas as pd
-import math
-from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+df = pd.read_csv("Life Expectancy Data.csv")
+st.title("Welcome to: Life Expectancy Predictions with Jared and Sonia!")
+st.write("Want to know how long you'll live? Look no further 😎")
+st.image("IMAGE.jpg")
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+from streamlit_option_menu import option_menu #pip install streamlit-option-menu
+selected = option_menu(menu_title=None, options=["01 Data", "02 Visualization", "03 Predictions"], default_index=0, orientation="horizontal")
+if selected == "01 Data":
+    st.markdown("### :violet[Let's take a look at the data!]")
+    st.image("globe.jpg")
+    num = st.number_input("No. of rows", 5,10)
+    st.dataframe(df.head(num))
+    st.image("politics.jpg")
+    st.dataframe(df.describe())
+    st.image("hi.png")
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+if selected== "02 Visualization":
+    import plotly.express as px
+    import streamlit as st
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import pandas as pd
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    numeric_columns = df.select_dtypes(include=["number"]).columns.tolist()
+    st.markdown("### :red[Data Visualization]")
+    selected_vars = st.multiselect("Select variables for the correlation matrix",numeric_columns,default=numeric_columns[:3])
+    st.title("Correlation Matrix of All Numerical Variables")
+    st.image("correlation.jpg")
+    st.write("Insights: The following correlation matrix displays the strength of the relationship between any two numerical variables in the dataset.")
+    if len (selected_vars)>1:
+        tab_corr,= st.tabs(["Correlation"])
+    correlation = df[selected_vars].corr()
+    fig = px.imshow(correlation.values, x=correlation.index, y=correlation.columns,labels=dict(color="Correlation"), text_auto=True)
+    tab_corr.plotly_chart(fig, theme="streamlit",use_container_width=True)
+   
+    st.title('Line Graph of Life Expectancy by Adult Mortality Rates')
+    st.write("Insights: The graph below clearly displays the strong, negative correlation between life expectancy and adult mortality rate. Although there is a unique bunch of outliers towards the left side of the graph, one can see clearly that overall, increases in adult mortality rates are associated with decreases in life expectancy.")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.lineplot(x='Adult Mortality',y='Life expectancy ', data=df, marker='o', ax=ax)
+    ax.set_title('Line Graph')
+    ax.set_xlabel('Adult Mortality Rate (probability of dying between 15 and 60 years per 1000 population)')
+    ax.set_ylabel('Life Expectancy in Age')
+    st.pyplot(fig)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    st.title('Violin Plot of Life Expectancy by County Status')
+    st.write("Insights: The following violin plot shows that life expectancies for individuals in developing countries are far more dispersed than those in developed countries. It appears that people in developed countries tend to live longer, and also have a mode life expectancy of around 80 years old, with the overwhelming majority of countries lying at about this age. People in developing countries seem to live shorter with a modal age of about 73, but do not have a strong pattern and there exists a lot more dispersion.")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.violinplot(x='Status', y='Life expectancy ', data=df, ax=ax)
+    ax.set_title('Violin Plot')
+    ax.set_xlabel('Status')
+    ax.set_ylabel('Life Expectancy')
+    st.pyplot(fig)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    st.title("Count Plot of Country Status")
+    st.write("Insights: The following count plot shows that most of the countries in this dataset are developing.")
+    ax.set_title('Count Plot')
+    fig, ax= plt.subplots(figsize=(8,5))
+    sns.countplot(x=df["Status"], data=df, ax=ax)
+    st.pyplot(fig)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+if selected== "03 Predictions":
+    import streamlit as st
+    import pandas as pd
+    from sklearn.metrics import r2_score, mean_absolute_error
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LinearRegression
+    from sklearn.impute import SimpleImputer
+    st.title("Life Expectancy Prediction Using Linear Regression")
+    st.write("Insights: The below scatterplot with a line of best fit demonstrates that using the numerical variables in the dataset, this linear regression model we generated is strong at predicting actual life expectancies in countries across time. There are few outliers and most of the datapoints lie close to the line. Additionally, the mean absolute error reflects that on average, the model is not too inaccurate in predicting life expectancies.")
+    
+    df = df.dropna()
+    X = df.drop(columns=['Life expectancy ', 'Country','Year','Status']) 
+    y = df['Life expectancy ']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    y_pred = lr.predict(X_test)
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+    fig, ax = plt.subplots(figsize=(8, 6))  # Create a figure and axis
+    sns.scatterplot(x=y_test, y=y_pred, alpha=0.5, ax=ax)  # Scatter plot
+    ax.set_xlabel("Actual Life Expectancies")  # X-axis label
+    ax.set_ylabel("Predicted Life Expectancies")  # Y-axis label
+    ax.set_title("Actual vs Predicted Life Expectancies")  # Plot title
+    ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], color='red', lw=2, ls='-')
+    st.pyplot(fig)
+    st.write(f"Mean Absolute Error (MAE): {mae:.2f}")
+    st.image("wonka.png")
